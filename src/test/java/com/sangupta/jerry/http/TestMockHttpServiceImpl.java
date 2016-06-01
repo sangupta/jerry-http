4c60a420a0ec25673b2ae4dc5c16bd366225654c
Sandeep Gupta <sandy.pec@gmail.com>
2016-06-01 23:25:22 +0530

    Completed tests for MockHttpServiceImpl

--- src/test/java/com/sangupta/jerry/http/TestMockHttpServiceImpl.java
+++ src/test/java/com/sangupta/jerry/http/TestMockHttpServiceImpl.java
@@ -1,11 +1,15 @@
 package com.sangupta.jerry.http;
 
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.sangupta.jerry.constants.HttpMimeType;
 import com.sangupta.jerry.http.mock.MockHttpServiceImpl;
 import com.sangupta.jerry.http.mock.MockWebResponse;
-import com.sangupta.jerry.util.StringUtils;
+import com.sangupta.jerry.util.ByteArrayUtils;
+import com.sangupta.jerry.util.HashUtils;
 
 /**
  * This class tests the behaviour of various methods of the
@@ -16,27 +20,29 @@ import com.sangupta.jerry.util.StringUtils;
  */
 public class TestMockHttpServiceImpl {
 	
+	private static final String SOME_TEST_URL = "http://localhost/someUrl";
+
 	private static final int MAX_LENGTH = 1024;
+	
+	private static final String RANDOM_STRING = HashUtils.getMD5Hex(ByteArrayUtils.getRandomBytes(MAX_LENGTH));
+	
+	private final MockHttpServiceImpl service = new MockHttpServiceImpl();
 	
 	@Test
 	public void testGetTextResponse() {
-		MockHttpServiceImpl service = new MockHttpServiceImpl();
-		String originalBody = StringUtils.getRandomString(MAX_LENGTH);
-		MockWebResponse response = new MockWebResponse(originalBody);
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
 		service.setNextResponse(response);
-		String body = service.getTextResponse("http://localhost/someUrl");
+		String body = service.getTextResponse(SOME_TEST_URL);
 		
 		Assert.assertNotNull(body);
-		Assert.assertEquals(originalBody, body);
+		Assert.assertEquals(RANDOM_STRING, body);
 	}
 	
 	@Test
 	public void testGetResponse() {
-		MockHttpServiceImpl service = new MockHttpServiceImpl();
-		String originalBody = StringUtils.getRandomString(MAX_LENGTH);
-		MockWebResponse response = new MockWebResponse(originalBody);
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
 		service.setNextResponse(response);
-		WebResponse result = service.getResponse("http://localhost/someUrl");
+		WebResponse result = service.getResponse(SOME_TEST_URL);
 		
 		Assert.assertNotNull(result);
 		Assert.assertEquals(response, result);
@@ -44,18 +50,136 @@ public class TestMockHttpServiceImpl {
 
 	@Test
 	public void testGetResponseHeaders() {
-		MockHttpServiceImpl service = new MockHttpServiceImpl();
-		String originalBody = StringUtils.getRandomString(MAX_LENGTH);
-		MockWebResponse response = new MockWebResponse(originalBody);
-		response.addHeader(originalBody, originalBody);
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
 		service.setNextResponse(response);
-		WebResponse result = service.getResponse("http://localhost/someUrl");
+		Map<String, String> result = service.getResponseHeaders(SOME_TEST_URL);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(1, result.size());
+		
+		Assert.assertEquals(RANDOM_STRING, result.get(RANDOM_STRING));
+	}
+	
+	@Test
+	public void testDoHEAD() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doHEAD(SOME_TEST_URL);
 		
 		Assert.assertNotNull(result);
 		Assert.assertEquals(response, result);
-		Assert.assertNotNull(response.getHeaders());
-		Assert.assertEquals(1, response.getHeaders().size());
-		
-		Assert.assertEquals(originalBody, response.getHeaders().get(originalBody));
 	}
+	
+	@Test
+	public void testDoGET() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doGET(SOME_TEST_URL);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testDoPOST() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doPOST(SOME_TEST_URL, RANDOM_STRING, HttpMimeType.BINARY);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testDoPUT() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doPUT(SOME_TEST_URL, RANDOM_STRING, HttpMimeType.BINARY);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testDoOPTIONS() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doOPTIONS(SOME_TEST_URL);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testDoTRACE() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doTRACE(SOME_TEST_URL);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testDoDELETE() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.doDELETE(SOME_TEST_URL);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testPostXML() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.postXML(SOME_TEST_URL, RANDOM_STRING);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testPostJSONL() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.postJSON(SOME_TEST_URL, RANDOM_STRING);
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testExecuteSilently() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.executeSilently(service.getWebRequest(WebRequestMethod.GET, SOME_TEST_URL));
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
+	@Test
+	public void testPlainExecuteSilently() {
+		MockWebResponse response = new MockWebResponse(RANDOM_STRING);
+		response.addHeader(RANDOM_STRING, RANDOM_STRING);
+		service.setNextResponse(response);
+		WebResponse result = service.plainExecuteSilently(service.getWebRequest(WebRequestMethod.GET, SOME_TEST_URL));
+		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(response, result);
+	}
+	
 }
